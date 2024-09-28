a1fb914434b841ad9f86b441a3c848cf25338065
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-09-27 23:32:07 -0700

    More

--- ai.py
+++ ai.py
@@ -3,11 +3,17 @@ import helpers as h
 all_moves = [h.move_up, h.move_down, h.move_left, h.move_right]
 
 
+def generate_score(board, current_depth=0, max_depth=4):
+    pass
+
+
 def simulate_move(board, move):
     new_board = move(board)
-    score = 0
 
-    return score
+    if new_board == board:
+        return 0
+    else:
+        return generate_score(new_board)
 
 
 def find_best_move(board):
